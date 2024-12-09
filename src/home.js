6ba4da1a08fa5b71216e8e6334b23b50a561a6a1
Kahnn <kahnnbaldock@outlook.com>
2024-12-09 14:11:30 +1300

    Added the webpack configuration to the project.

---
+++ src/home.js
@@ -0,0 +1,6 @@
+//Entry point to the to do list webpage
+function createHome() {
+    console.log("Hone is created");
+}
+
+export default createHome();
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles.css";
+import createHome from './home.js';
+
+createHome;
